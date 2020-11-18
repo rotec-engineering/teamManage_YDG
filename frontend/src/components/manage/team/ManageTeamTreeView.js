7550b821d010e5874bd29c2824803c88fdff1253
ydg8567 <ydg8567@naver.com>
2020-11-18 17:27:44 +0900

    make component

---
+++ frontend/src/components/manage/team/ManageTeamTreeView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ManageTeamTreeView.css';
+
+const ManageTeamTreeView = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ManageTeamTreeView;
